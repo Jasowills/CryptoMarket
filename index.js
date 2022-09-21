2140f78bc43867e153310b56905dc620ace51108
Jasowills <Jujuzhaddy01@gmail.com>
2022-09-21 16:25:27 +0100

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 import 'charts.css';
+import 'animate.css';
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
